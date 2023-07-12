6086d730dcb754f37f4e850705242c981f3e0a61
DerBismarck <pankgaming@gmail.com>
2023-07-12 14:34:45 -0500

    Invert these checks to handle null value, fixes first shot headshot kills not working

--- Coop/Player/Player_ApplyDamageInfo_Patch.cs
+++ Coop/Player/Player_ApplyDamageInfo_Patch.cs
@@ -30,9 +30,9 @@ namespace SIT.Core.Coop.Player
             , ref DamageInfo damageInfo
             , EBodyPart bodyPartType)
         {
-            var result = false;
-            if (CallLocally.TryGetValue(__instance.Profile.AccountId, out var expecting) && expecting)
-                result = true;
+            var result = true;
+            if (CallLocally.TryGetValue(__instance.Profile.AccountId, out var expecting) && !expecting)
+                result = false;
             if(result)
             {
                 if (PluginConfigSettings.Instance != null)
